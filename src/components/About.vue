5255a004a76f9bf56afa9aa3bff9dd19a80b5bed
Ana Jauregui <ana.jauregui@techtonic.com>
2019-03-12 09:49:47 -0600

    init commit

---
+++ src/components/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <h1>Hello {{ their_name }}, This is an about page</h1>
+    <p>Lorm ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempur incididunt</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Skills',
+    data() {
+      return {
+        their_name: this.$route.params.name
+      }
+    }
+  }
+</script>
